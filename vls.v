17245ba961ae7acd5895d253dcffdd249464b919
Ned Palacios <npdoesmc@gmail.com>
2020-11-11 20:22:49 +0800

    update vls.v

--- vls.v
+++ vls.v
@@ -6,34 +6,44 @@ import strings
 import jsonrpc
 import json
 // import x.json2
-import v.ast
-import v.checker
-import v.table
 import v.pref
 import v.doc
+import v.ast
+import v.token
+import v.table
 import v.fmt
-import v.scanner
+import v.checker
+
+// TODO: free strings
 
 const (
 	log_file = os.resource_abs_path('output.txt')
+	vls_prefs = &pref.Preferences{ 
+		output_mode: .silent
+		enable_globals: true
+	}
+	global_scope = &ast.Scope{ parent: 0 }
+	scope = &ast.Scope{
+		start_pos: 0
+		parent: global_scope
+	}
 )
 
-// TODO: ignore version in files for now.
 struct Vls {
 mut:
-	status ServerStatus = .initializing
-	file_contents map[string]string
-	doc_nodes map[string][]doc.DocNode
-	scanned map[string][]&scanner.Scanner
-	files map[string]ast.File
-	prefs  &pref.Preferences = &pref.Preferences{ output_mode: .silent }
+	log_requests bool
 	table &table.Table = table.new_table()
-	doc doc.Doc
-	checker checker.Checker
-	// for completion
-	saved_completion lsp.CompletionList
-	saved_offset int = -1
-	saved_filename string
+	status ServerStatus = .initializing
+	// imports
+	import_graph map[string][]string
+	mod_import_paths map[string]string
+	mod_docs map[string]doc.Doc
+	// directory -> file name
+	projects map[string]Project
+	docs map[string]doc.Doc
+	tokens map[string]map[string][]token.Token
+	asts map[string]map[string]ast.File
+	current_file string
 }
 
 enum ServerStatus {
@@ -42,197 +52,43 @@ enum ServerStatus {
 	shutdown
 }
 
-fn (vls Vls) compute_offset(file_path string, line int, col int) int {
-	text := vls.file_contents[file_path]
-	lines := text.split_into_lines()
-	mut offset := 0
-
-	for i, ln in lines {
-		if i == line {
-			if col > ln.len-1 {
-				return -1	
-			}
-			if ln.len == 0 {
-				offset++
-				break
-			}
-			offset += col
-			break
-		} else {
-			offset += ln.len+1
-		}
+fn (mut vls Vls) new_project(project_dir string) {
+	vls.projects[project_dir] = Project{
+		path: project_dir
+		active_files: []string{}
+		cached_symbols: []lsp.SymbolInformation{}
+		cached_completion: []lsp.CompletionItem{}
+		nr_errors: 0
 	}
-
-	return offset
+	vls.docs[project_dir] = doc.Doc{ 
+		base_path: project_dir
+		prefs: vls_prefs
+		pub_only: false
+		with_head: false
+		extract_vars: true
+		fmt: fmt.Fmt{
+			indent: 0
+			is_debug: false
+			table: vls.table
+		}
+		sources: map[string]string{}
+		checker: checker.new_checker(vls.table, vls_prefs)
+	}
+	vls.tokens[project_dir] = map[string][]token.Token{}
+	vls.asts[project_dir] = map[string]ast.File{}
 }
 
-fn (mut vls Vls) completion(id int, raw string) {
-	// use position for now
-	params := json.decode(lsp.CompletionParams, raw) or {
-		emit_parse_error()
-		return
+fn (mut vls Vls) shutdown() {
+	vls.status = .shutdown
+	unsafe {
+		vls.projects.free()
+		vls.mod_import_paths.free()
+		vls.import_graph.free()
+		vls.mod_docs.free()
 	}
-	ctx := params.context
-	pos := params.position
-	fs_path := get_fspath_from_uri(params.text_document.uri)
-	if fs_path.len == 0 {
-		return
-	}
-	file_ast := vls.files[fs_path]
-	offset := vls.compute_offset(fs_path, pos.line, pos.character)
-
-	// TODO: will be removed once there is support for on-demand computation
-	if ctx.trigger_kind == .invoked && offset != -1 {
-		mut items := []lsp.CompletionItem{}
-		doc_nodes := vls.doc.generate_from_ast_with_pos(file_ast, offset)
-		for node in doc_nodes {
-			kind := match node.attrs['category'] {
-				'Constants' {
-					lsp.CompletionItemKind.constant
-				}
-				'Enums' {
-					lsp.CompletionItemKind.@enum
-				}
-				'Interfaces' {
-					lsp.CompletionItemKind.@interface
-				}
-				'Structs' {
-					lsp.CompletionItemKind.@struct
-				}
-				'Typedefs' {
-					lsp.CompletionItemKind.type_parameter
-				}
-				'Functions',
-				'Methods' {
-					lsp.CompletionItemKind.function
-				}
-				'Variables' {
-					lsp.CompletionItemKind.variable
-				}
-				else {
-					lsp.CompletionItemKind.field
-				}
-			}
-			items << lsp.CompletionItem{
-				label: node.name
-				kind: kind
-				detail: node.attrs['return_type']
-				documentation: lsp.MarkupContent{
-					kind: 'markdown'
-					value: node.comment
-				}
-				deprecated: false
-			}
-		}
-
-		vls.saved_offset = offset
-		vls.saved_filename = fs_path
-		vls.saved_completion = lsp.CompletionList{
-			is_incomplete: false
-			items: items
-		}
-	}
-	respond(json.encode(JrpcResponse<lsp.CompletionList>{
-		id: id
-		result: vls.saved_completion
-	}))
-}
-
-// TODO: use scanner for it
-fn (vls Vls) offset_to_doc_pos(file_path string, start_pos, end_pos int) doc.DocPos {
-	text := vls.file_contents[file_path]
-	if start_pos > text.len || end_pos > text.len {
-		return doc.DocPos{-1, -1, 0}
-	}
-
-	lines := text.split_into_lines()
-	len := end_pos - start_pos
-	mut line := -1
-	mut col := -1
-	mut total := start_pos
-	mut has_pos := false
-
-	for i, ln in lines {
-		for j in 0..ln.len {
-			total--
-			if total == 0 {
-				line = i
-				col = j
-				has_pos = true
-				break
-			}
-		}
-		if has_pos {
-			break
-		}
-	}
-
-	// line - col - len
-	return doc.DocPos{line, col, len}
-}
-
-fn (mut vls Vls) hover(id int, raw string) {
-	params := json.decode(lsp.HoverParams, raw) or {
-		emit_parse_error()
-		return
-	}
-
-	fs_path := get_fspath_from_uri(params.text_document.uri)
-	if fs_path.len == 0 {
-		return
-	}
-
-	pos := params.position
-	offset := vls.compute_offset(fs_path, pos.line, pos.character)
-	show_message(.info, offset.str())
-	if offset == -1 {
-		return
-	}
-
-	text := vls.file_contents[fs_path]
-	mut start_pos := offset
-	mut end_pos := offset
-
-	for {
-		c := text[end_pos]
-		if (c.is_letter() || c.is_digit() || c == `_`) && end_pos < text.len {
-			end_pos++
-			continue
-		}
-		break
-	}
-
-	for {
-		c := text[start_pos]
-		if (c.is_letter() || c.is_digit() || c == `_`) && start_pos > 0 {
-			start_pos--
-			continue
-		}
-		break
-	}
-
-	doc_pos := vls.offset_to_doc_pos(fs_path, start_pos, end_pos)
-	if doc_pos.line == -1 && doc_pos.col == -1 {
-		return
-	}
-
-	line := text.split_into_lines()[doc_pos.line].trim_space()
-	if line.starts_with('//') || line.starts_with('/*') {
-		return
-	}
-
-	range := doc_pos_to_lsp_range({ doc_pos | line: doc_pos.line+1 })
-	log_message(.info, range.str())
-	respond(json.encode(JrpcResponse<lsp.Hover>{
-		id: id
-		result: lsp.Hover{
-			contents: lsp.MarkedString{
-				language: 'v'
-				value: text[start_pos..end_pos]
-			}
-			range: range
-		}
-	}))
+	// move exit to shutdown for now
+	show_message(.log, 'Goodbye!')
+	exit(int(vls.status != .shutdown))
 }
 
 fn (mut vls Vls) initialize(id int, raw string) {
@@ -240,30 +96,19 @@ fn (mut vls Vls) initialize(id int, raw string) {
 	// 	emit_parse_error()
 	// 	return
 	// }
-
-	// init_map_capa := init.as_map()['capabilities'].as_map()
-	// workspace_capa := init_map_capa['workspace'].as_map()
-	// doc_capa := init_map_capa['text_document'].as_map() 
-
 	// TODO: focus on capabilities for now
 	mut server_capabilities := lsp.ServerCapabilities{
-		text_document_sync: 1 // send full content on each revision for now.
+		text_document_sync: 1
 		// hover_provider: true
 		workspace_symbol_provider: true
 		document_symbol_provider: true
 	}
 
-	// if workspace_capa['symbol'].as_map()['dynamic_registration'].bool() {
-		server_capabilities.code_lens_provider.resolve_provider = false
-	// }
+	server_capabilities.completion_provider.resolve_provider = false
+	// TODO:
+	// server_capabilities.signature_help_provider.trigger_characters = ['(', '{']
+	// server_capabilities.signature_help_provider.trigger_characters = [',']
 
-	// if doc_capa['publish_diagnostics'].as_map()['related_information'].bool() {
-		// TODO: options for diagnostics
-	// }
-
-	// if doc_capa.completion.dynamic_registration {
-		// server_capabilities.completion_provider.resolve_provider = false
-	// }
 	respond(json.encode(JrpcResponse<lsp.InitializeResult>{
 		id: id,
 		result: lsp.InitializeResult{
@@ -275,85 +120,65 @@ fn (mut vls Vls) initialize(id int, raw string) {
 fn (mut vls Vls) execute(payload string) {
 	request := json.decode(jsonrpc.Request, payload) or {
 		emit_parse_error()
-		return
+		exit(1)
 	}
-
+	if vls.log_requests {
+		log_contents := os.read_file(log_file) or { '' }
+		os.write_file(log_file, log_contents + payload + '\n')
+	}
 	if request.method != 'exit' && vls.status == .shutdown {
 		emit_error(jsonrpc.invalid_request)
 		return
 	}
-
 	match request.method {
 		'initialize' { vls.initialize(request.id, request.params) }
 		'initialized' { vls.status = .initializing }
-		'shutdown' { vls.status = .shutdown }
-		'exit' {
-			show_message(.log, 'Goodbye!')
-			exit(int(vls.status != .shutdown))
-		}
-		'textDocument/didOpen' { vls.open_file(request.id, request.params) }
-		'textDocument/didSave' { vls.save_file(request.id, request.params) }
-		'textDocument/didClose' { vls.close_file(request.id, request.params) }
+		'shutdown' { vls.shutdown() }
+		'exit' { /* ignore */ }
+		'workspace/didChangeWatchedFiles' { vls.did_change_watched_files(request.params) }
 		'workspace/symbol' { vls.workspace_symbol(request.id, request.params) }
+		'textDocument/didOpen' { vls.open_file(request.params) }
+		'textDocument/didChange' { vls.change_file(request.params) }
+		'textDocument/didSave' { vls.save_file(request.params) }
+		'textDocument/didClose' { vls.close_file(request.params) }
 		'textDocument/documentSymbol' { vls.document_symbol(request.id, request.params) }
-		// 'textDocument/completion' { vls.completion(request.id, request.params) }
-		// 'textDocument/hover' { vls.hover(request.id, request.params) }
+		'textDocument/completion' { vls.completion(request.id, request.params) }
+		'textDocument/hover' { vls.hover(request.id, request.params) }
+		// 'textDocument/signatureHelp' { vls.signature_help(request.id, request.params) }
 		else {
 			if vls.status != .initialized {
 				emit_error(jsonrpc.server_not_initialized)
-				return
 			}
+			return
 		}
 	}
 }
 
 fn (mut vls Vls) start_loop() {
-	log_requests := os.getenv('VLS_LOG') == '1' || '-log' in os.args
-
 	for {
 		first_line := get_raw_input()
 		if first_line.len < 1 || !first_line.starts_with(content_length) {
 			continue
 		}
-
+		mut buf := strings.new_builder(1)
 		mut conlen := first_line[content_length.len..].int()
-		$if !windows {
-			conlen = conlen + 1
-		}
-		mut buf := strings.new_builder(200)
-
+		$if !windows { conlen++ }
 		for conlen > 0 {
-			mut c := C.fgetc(C.stdin)
+			c := C.fgetc(C.stdin)
 			$if !windows {
 				if c == 10 { continue }
 			}
 			buf.write_b(byte(c))
 			conlen--
 		}
-
 		payload := buf.str()
-		if log_requests {
-			log_contents := os.read_file(log_file) or { '' }
-			os.write_file(log_file, log_contents + payload[1..] + '\n')
-		}
 		vls.execute(payload[1..])
+		unsafe { buf.free() }
 	}
 }
 
 fn main() {
 	mut vls := Vls{}
-	vls.checker = checker.new_checker(vls.table, vls.prefs)
-	vls.doc = doc.Doc{
-		table: vls.table
-		prefs: vls.prefs
-		checker: vls.checker
-		pub_only: false
-		with_head: false
-		fmt: fmt.Fmt{
-			indent: 0
-			is_debug: false
-			table: vls.table
-		}
-	}
+	vls.log_requests = os.getenv('VLS_LOG') == '1' || '-log' in os.args
 	vls.start_loop()
 }
