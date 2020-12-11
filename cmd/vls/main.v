7c29c7d3e23d617f8c49336109cb54eb456e1b90
Ned Palacios <npdoesmc@gmail.com>
2020-12-11 22:11:13 +0800

    start_loop -> start_server

--- cmd/vls/main.v
+++ cmd/vls/main.v
@@ -4,11 +4,7 @@ import vls
 
 fn main() {
 	mut ls := vls.Vls{
-		send: fn (data string) {
-			// print to stdout
-			print('Content-Length: ${data.len}\r\n\r\n$data')
-		}
 		// logging: os.getenv('VLS_LOG') == '1' || '-log' in os.args
 	}
-	ls.start_loop()
+	ls.start_server()
 }
